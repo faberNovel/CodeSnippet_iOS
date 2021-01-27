994a41969415a88c27307e32b07a01f8e853ac15
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2021-01-27 15:58:09 +0100

    Create SeparatorView

---
+++ XCTemplate/UI/View/ADSeparatorView.xctemplate/SeparatorView.swift
@@ -0,0 +1,79 @@
+//___FILEHEADER___
+
+import UIKit
+
+class SeparatorView: UIView {
+
+    var edge: UIRectEdge = .bottom {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    var separatorColor: UIColor = .black {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    var separatorHeight: CGFloat = 1.0 {
+        didSet {
+            setNeedsDisplay()
+        }
+    }
+
+    // MARK: - Life Cycle
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpView()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setUpView()
+    }
+
+    // MARK: - UIView
+
+    override func draw(_ rect: CGRect) {
+        separatorColor.setFill()
+        UIRectFill(rect)
+        UIRectClip(clipppingRect(for: edge, in: rect))
+        UIColor.clear.setFill()
+        UIRectFill(rect)
+    }
+
+    override var intrinsicContentSize: CGSize {
+        CGSize(width: UIView.noIntrinsicMetric, height: max(separatorHeight, 1))
+    }
+
+    // MARK: - Private
+
+    private func setUpView() {
+        backgroundColor = .clear
+    }
+
+    private func clipppingRect(for edge: UIRectEdge, in rect: CGRect) -> CGRect {
+        var width = rect.width
+        if edge.contains(.left) {
+            width -= separatorHeight
+        }
+        if edge.contains(.right) {
+            width -= separatorHeight
+        }
+        var height = rect.height
+        if edge.contains(.top) {
+            height -= separatorHeight
+        }
+        if edge.contains(.bottom) {
+            height -= separatorHeight
+        }
+        return CGRect(
+            x: edge.contains(.left) ? separatorHeight : 0,
+            y: edge.contains(.top) ? separatorHeight : 0,
+            width: width,
+            height: height
+        )
+    }
+}

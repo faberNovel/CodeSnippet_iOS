6b82faa83fd769fd45895fbb755b029104dc6e44
Gaétan Zanella <gaetan.zanella@fabernovel.com>
2021-01-28 16:43:27 +0100

    Create ADButton template

---
+++ XCTemplate/UI/View/ADButton.xctemplate/ADButton.swift
@@ -0,0 +1,89 @@
+//___FILEHEADER___
+
+import UIKit
+
+/// A `UIButton` subclass working gracefully with `ViewStyle`.
+///
+/// `ViewStyle.style` will be called multiple times, possibly in animation blocks, when the button state changes.
+final class <#AD#>Button: UIButton {
+
+    private var style: ViewStyle<HCButton>?
+
+    private var expectedIntrinsicContentSize: CGSize?
+
+    // MARK: - UIView
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        style?.style(self)
+    }
+
+    override func tintColorDidChange() {
+        super.tintColorDidChange()
+        setNeedsLayout()
+    }
+
+    override var isHighlighted: Bool {
+        get {
+            super.isHighlighted
+        }
+        set {
+            animateStateChange {
+                super.isHighlighted = newValue
+            }
+        }
+    }
+
+    override var isEnabled: Bool {
+        get {
+            super.isEnabled
+        }
+        set {
+            animateStateChange {
+                super.isEnabled = newValue
+            }
+        }
+    }
+
+    override var intrinsicContentSize: CGSize {
+        if let size = expectedIntrinsicContentSize {
+            return size
+        }
+        return super.intrinsicContentSize
+    }
+
+    // MARK: - ViewStylable
+
+    convenience init(style: ViewStyle<HCButton>) {
+        self.init(type: .custom)
+        applyStyle(style)
+        setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        setContentHuggingPriority(.defaultHigh, for: .vertical)
+        setContentCompressionResistancePriority(.defaultHigh, for: .vertical)
+        setContentCompressionResistancePriority(.defaultHigh, for: .horizontal)
+    }
+
+    func applyStyle(_ style: ViewStyle<HCButton>) {
+        self.style = style
+        style.style(self)
+        setNeedsLayout()
+    }
+
+    // MARK: - Public
+
+    func setIntrinsicContentSize(_ size: CGSize) {
+        guard size != expectedIntrinsicContentSize else { return }
+        expectedIntrinsicContentSize = size
+        invalidateIntrinsicContentSize()
+    }
+
+    // MARK: - Private
+
+    private func animateStateChange(stateUpdate: () -> Void) {
+        layoutIfNeeded()
+        stateUpdate()
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            self?.layoutIfNeeded()
+        }
+    }
+}
